10e32ec9fc1b95f2628ffa9412a41ba36a9542df
Manish Choubisa <choubisa.manish@gmail.com>
2025-07-29 12:43:31 +0530

    Error findings

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,11 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& readings ) {
+
     //Implement statistics here
+    Stats stats;
+    stats.average = std::accumulate(readings.begin(), readings.end(), 0.0f) / readings.size();
+    stats.max = *std::max_element(readings.begin(), readings.end());
+    stats.min = *std::min_element(readings.begin(), readings.end());
+    return stats;
 }
